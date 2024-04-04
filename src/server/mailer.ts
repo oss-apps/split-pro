ec120ddc7f58c3f80a5ea245508d4bf7fd96788a
KMKoushik <koushikmohan1996@gmail.com>
2024-04-04 20:04:10 +1100

    Fix typo

--- src/server/mailer.ts
+++ src/server/mailer.ts
@@ -36,7 +36,7 @@ export async function sendInviteEmail(email: string, name: string) {
     to: email,
     subject: 'Invitation to SplitPro',
     text: `Hey,\n\nYou have been invited to SplitPro by ${name}. It's a completely open source free alternative to splitwise. You can sign in to SplitPro by clicking the below URL:\n${env.NEXTAUTH_URL}\n\nThanks,\nKoushik KM\nSplitPro`,
-    html: `<p>Hey,</p> <p>You have been invited to SplitPro by ${name}. It's a completely open source free alternative to splitwise. You can sign in to SplitPro by clicking the below URL:</p><p><a href="${env.NEXTAUTH_URL}">Sign in to ${host}</a></p><p>Thanks,<br/>Koushik KM<br/>SplitPro</p>`,
+    html: `<p>Hey,</p> <p>You have been invited to SplitPro by ${name}. It's a completely open source free alternative to splitwise. You can sign in to SplitPro by clicking the below URL:</p><p><a href="${env.NEXTAUTH_URL}">Sign in to ${host}</a></p><br><p>Thanks,<br/>Koushik KM<br/>SplitPro</p>`,
   });
 
   console.log('Email sent', response);

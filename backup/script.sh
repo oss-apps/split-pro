003315bf219c7596b337e120f64ed6a63b33a786
KM Koushik <koushikmohan1996@gmail.com>
2024-09-28 10:28:03 +1000

    hacktober cleanup (#111)

--- backup/script.sh
+++
@@ -1,9 +0,0 @@
-base="/root/.bps"
-folder=$(date +%b-%d-%Y)
-file=$(date +%H-%M-%S)
-
-mkdir -p ${base}/${folder}
-
-docker exec split-pro-pg pg_dump -U postgres -F p postgres > ${base}/${folder}/${file}.sql
-
-echo "Splitpro backup on ${folder}" | mutt -s "Splitpro backup ${folder}" target@email.com -a ${base}/${folder}/${file}.sql

0363080c739eb05e31ee45292fdbaf507a874c2d
Nicolas Ross <nicolas@cybercat.ca>
2024-12-13 15:12:08 -0500

    Missing closing angle bracket (#154)

--- src/server/mailer.ts
+++ src/server/mailer.ts
@@ -46,7 +46,7 @@ export async function sendSignUpEmail(email: string, token: string, url: string)
 
   const subject = 'Sign in to SplitPro';
   const text = `Hey,\n\nYou can sign in to SplitPro by clicking the below URL:\n${url}\n\nYou can also use this OTP: ${token}\n\nThanks,\nSplitPro Team`;
-  const html = `<p>Hey,</p> <p>You can sign in to SplitPro by clicking the below URL:</p><p><a href="${url}">Sign in to ${host}</a></p><p>You can also use this OTP: <b>${token}</b></p<br /><br /><p>Thanks,</p><br/>SplitPro Team</p>`;
+  const html = `<p>Hey,</p> <p>You can sign in to SplitPro by clicking the below URL:</p><p><a href="${url}">Sign in to ${host}</a></p><p>You can also use this OTP: <b>${token}</b></p><br /><br /><p>Thanks,</p><br/>SplitPro Team</p>`;
 
   return await sendMail(email, subject, text, html);
 }

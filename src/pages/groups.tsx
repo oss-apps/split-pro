48deba1ecb4cc4cdd7fc0c11e23b8adc93a9633e
Wiktor Krokosz <38408316+krokosik@users.noreply.github.com>
2025-05-24 11:53:04 +0200

    Fix: group balance showing empty currency (#204)
    * Show the biggest total balance in group summary
    
    * Sort and remove unused imports
    
    * Show an asterisk for multi currency balances

--- src/pages/groups.tsx
+++ src/pages/groups.tsx
@@ -1,17 +1,15 @@
-import Head from 'next/head';
-import MainLayout from '~/components/Layout/MainLayout';
-import clsx from 'clsx';
-import { Button } from '~/components/ui/button';
 import { PlusIcon } from '@heroicons/react/24/solid';
-import { getServerAuthSessionForSSG } from '~/server/auth';
-import { type User } from '@prisma/client';
-import { api } from '~/utils/api';
-import { CreateGroup } from '~/components/group/CreateGroup';
-import Link from 'next/link';
-import { GroupAvatar } from '~/components/ui/avatar';
-import { toUIString } from '~/utils/numbers';
+import clsx from 'clsx';
 import { motion } from 'framer-motion';
+import Head from 'next/head';
+import Link from 'next/link';
+import { CreateGroup } from '~/components/group/CreateGroup';
+import MainLayout from '~/components/Layout/MainLayout';
+import { GroupAvatar } from '~/components/ui/avatar';
+import { Button } from '~/components/ui/button';
 import { type NextPageWithUser } from '~/types';
+import { api } from '~/utils/api';
+import { toUIString } from '~/utils/numbers';
 
 const BalancePage: NextPageWithUser = () => {
   const groupQuery = api.group.getAllGroupsWithBalances.useQuery();
@@ -47,9 +45,16 @@ const BalancePage: NextPageWithUser = () => {
               </motion.div>
             ) : (
               groupQuery.data?.map((g) => {
-                const [amount, currency] = Object.keys(g.balances).length
-                  ? [Object.values(g.balances)[0] ?? 0, Object.keys(g.balances)[0] ?? 'USD']
-                  : [0, 'USD'];
+                const [currency, amount] = Object.entries(g.balances).reduce(
+                  (acc, balance) => {
+                    if (Math.abs(balance[1]) > Math.abs(acc[1])) {
+                      return balance;
+                    }
+                    return acc;
+                  },
+                  [g.defaultCurrency, 0],
+                );
+                const multiCurrency = Object.values(g.balances).filter((b) => b !== 0).length > 1;
                 return (
                   <GroupBalance
                     key={g.id}
@@ -58,6 +63,7 @@ const BalancePage: NextPageWithUser = () => {
                     amount={amount}
                     isPositive={amount >= 0 ? true : false}
                     currency={currency}
+                    multiCurrency={multiCurrency}
                   />
                 );
               })
@@ -75,7 +81,8 @@ const GroupBalance: React.FC<{
   amount: number;
   isPositive: boolean;
   currency: string;
-}> = ({ name, amount, isPositive, currency, groupId }) => {
+  multiCurrency?: boolean;
+}> = ({ name, amount, isPositive, currency, groupId, multiCurrency }) => {
   return (
     <Link href={`/groups/${groupId}`}>
       <div className="flex items-center justify-between">
@@ -98,6 +105,7 @@ const GroupBalance: React.FC<{
               </div>
               <div className={`${isPositive ? 'text-emerald-500' : 'text-orange-600'} text-right`}>
                 {currency} {toUIString(amount)}
+                {multiCurrency ? '*' : ''}
               </div>
             </>
           )}

b53b7b0411889d4d283041d9f91d38fff6642368
krokosik <wikkra@gmail.com>
2025-05-26 23:23:20 +0200

    Convert the SQL types to bigint

---
+++ prisma/migrations/20250526193958_migrate_amounts_to_bigint/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "Balance" ALTER COLUMN "amount" SET DATA TYPE BIGINT;
+
+-- AlterTable
+ALTER TABLE "Expense" ALTER COLUMN "amount" SET DATA TYPE BIGINT;
+
+-- AlterTable
+ALTER TABLE "ExpenseParticipant" ALTER COLUMN "amount" SET DATA TYPE BIGINT;
+
+-- AlterTable
+ALTER TABLE "GroupBalance" ALTER COLUMN "amount" SET DATA TYPE BIGINT;

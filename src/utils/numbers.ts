1dc4863dd4ce3e42326d28e665721911c445783e
krokosik <wikkra@gmail.com>
2025-05-26 23:23:47 +0200

    Create new bigint compatible number utils

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -1,14 +1,54 @@
-export function toFixedNumber(num: number) {
-  return num / 100;
+export function toSafeBigInt(num: number | string) {
+  if (typeof num === 'number') {
+    return BigInt(Math.round(num * 100));
+  } else if (typeof num === 'string') {
+    const parsed = parseFloat(num);
+    if (isNaN(parsed)) {
+      throw new Error(`Invalid number string: ${num}`);
+    }
+    const num_unified_decimal = num.replace(',', '.');
+    const parts = num_unified_decimal.split('.');
+    const whole = BigInt(parts[0]!);
+    const fraction = BigInt(Math.round(parseFloat(`0.${parts[1] ?? '0'}`) * 100));
+
+    return whole * 100n + fraction;
+  } else {
+    throw new Error(`Unsupported type: ${typeof num}`);
+  }
 }
 
-export function toInteger(num: number) {
-  return Math.round(num * 100);
+export function toUIString(num: bigint) {
+  const absNum = BigMath.abs(num);
+  return `${absNum.toString().slice(0, -2) || '0'}.${absNum.toString().slice(-2).padStart(2, '0')}`;
 }
 
-export function toUIString(num: number) {
-  return toFixedNumber(Math.abs(num)).toLocaleString(undefined, {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
-}
+export const BigMath = {
+  abs(x: bigint) {
+    return x < 0n ? -x : x;
+  },
+  sign(x: bigint) {
+    if (x === 0n) return 0n;
+    return x < 0n ? -1n : 1n;
+  },
+  pow(base: bigint, exponent: bigint) {
+    return base ** exponent;
+  },
+  min(...values: bigint[]) {
+    if (values.length === 0) {
+      return 0n;
+    } else {
+      let value = values[0]!;
+      for (const v of values) if (v < value) value = v;
+      return value;
+    }
+  },
+  max(...values: bigint[]) {
+    if (values.length === 0) {
+      return 0n;
+    } else {
+      let value = values[0]!;
+      for (const v of values) if (v > value) value = v;
+      return value;
+    }
+  },
+};

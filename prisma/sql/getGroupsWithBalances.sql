beb89daaecbb7dfd3d720ad8b83d2103ddaa986e
Jasper Behrensdorf <jasper.behrensdorf@gmail.com>
2025-06-07 18:34:14 +0200

    Return groups without expenses

--- prisma/sql/getGroupsWithBalances.sql
+++ prisma/sql/getGroupsWithBalances.sql
@@ -1,8 +1,8 @@
 -- @param {Int} $1: id of the user
-SELECT "Group"."id", "Group".name, sum("ExpenseParticipant".amount), "Expense".currency
+SELECT "Group"."id", "Group".name, Coalesce(sum("ExpenseParticipant".amount),0) as balance, Coalesce("Expense".currency, "Group"."defaultCurrency") as currency
 FROM "GroupUser"
 JOIN "Group" ON "GroupUser"."groupId" = "Group".id
-JOIN "Expense" ON "Expense"."groupId" = "Group".id
-JOIN "ExpenseParticipant" ON "Expense".id = "ExpenseParticipant"."expenseId"
-WHERE "GroupUser"."userId" = $1 AND "deletedAt" IS NULL AND "ExpenseParticipant"."userId" = $1
+LEFT JOIN "Expense" ON "Expense"."groupId" = "Group".id
+LEFT JOIN "ExpenseParticipant" ON "Expense".id = "ExpenseParticipant"."expenseId"
+WHERE "GroupUser"."userId" = $1 AND "deletedAt" IS NULL AND ("ExpenseParticipant"."userId" = $1 OR "Expense".id is null)
 GROUP BY "Group".id, "Group".name, "Expense".currency

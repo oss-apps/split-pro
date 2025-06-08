df6a7ad8959cf7462edbf45f9f95c1f1db459982
Jasper Behrensdorf <jasper.behrensdorf@gmail.com>
2025-06-08 02:28:58 +0200

    Order groups descending by newest

--- prisma/sql/getGroupsWithBalances.sql
+++ prisma/sql/getGroupsWithBalances.sql
@@ -1,4 +1,4 @@
--- @param {Int} $1: id of the user
+-- @param {Int} $1:id of the user
 SELECT "Group"."id", "Group".name, Coalesce(sum("ExpenseParticipant".amount),0) as balance, Coalesce("Expense".currency, "Group"."defaultCurrency") as currency
 FROM "GroupUser"
 JOIN "Group" ON "GroupUser"."groupId" = "Group".id
@@ -6,3 +6,4 @@ LEFT JOIN "Expense" ON "Expense"."groupId" = "Group".id
 LEFT JOIN "ExpenseParticipant" ON "Expense".id = "ExpenseParticipant"."expenseId"
 WHERE "GroupUser"."userId" = $1 AND "deletedAt" IS NULL AND ("ExpenseParticipant"."userId" = $1 OR "Expense".id is null)
 GROUP BY "Group".id, "Group".name, "Expense".currency
+ORDER BY "Group"."createdAt" DESC, balance DESC

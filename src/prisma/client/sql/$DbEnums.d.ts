fa22c438ad32ded2c33471f5ed4df0fd5c5981ec
Jasper Behrensdorf <jasper.behrensdorf@gmail.com>
2025-06-10 01:11:50 +0200

    Remove sql types

--- src/prisma/client/sql/$DbEnums.d.ts
+++
@@ -1,6 +0,0 @@
-export interface $DbEnums {}
-
-
-export namespace $DbEnums {
-  type SplitType = "EQUAL" | "PERCENTAGE" | "EXACT" | "SHARE" | "ADJUSTMENT" | "SETTLEMENT"
-}
--- src/prisma/client/sql/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { $DbEnums } from "./$DbEnums"
-
-export * from "./getAllBalancesForGroup"
-
-export * from "./getGroupsWithBalances"

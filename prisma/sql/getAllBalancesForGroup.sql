c26dc4ab3adb477ed1a38e20f64bb1c3cb90d263
Jasper Behrensdorf <jasper.behrensdorf@gmail.com>
2025-06-10 17:17:30 +0200

    Exclude deleted expenses from group balance

--- prisma/sql/getAllBalancesForGroup.sql
+++ prisma/sql/getAllBalancesForGroup.sql
@@ -1,6 +1,6 @@
 -- @param {Int} $1:id of the group
 select "groupId", "userId" as "borrowedBy", "paidBy", "currency", CAST(Coalesce(-1 * sum("ExpenseParticipant".amount),0) as BIGINT) as amount FROM "Expense"
 JOIN "ExpenseParticipant" ON "ExpenseParticipant"."expenseId" = "Expense".id
-WhERE "groupId" = $1 AND "userId" != "paidBy"
+WhERE "groupId" = $1 AND "userId" != "paidBy" AND "Expense"."deletedAt" IS NULL
 GROUP BY "userId", "paidBy", "currency", "groupId"
 ORDER By "currency"

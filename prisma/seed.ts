06eea7839ae359872eac93472fb734c64a5dc39c
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2025-06-18 13:28:53 +0200

    Update seed
    1000 users in 101 groups with 10k expenses each

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,116 +4,118 @@ import { randomInt } from 'crypto';
 const prisma = new PrismaClient();
 
 async function createUsers() {
+  const data = Array.from({ length: 1000 }, (value, index) => index).map(i => {
+    return {
+      name: `user${i}`,
+      email: `user${i}@example.com`,
+      currency: 'USD'
+    }
+  })
   const users = await prisma.user.createMany({
-    data: [
-      {
-        name: 'Alice',
-        email: 'alice@example.com',
-        currency: 'USD',
-      },
-      {
-        name: 'Bob',
-        email: 'bob@example.com',
-        currency: 'EUR',
-      },
-      {
-        name: 'Charlie',
-        email: 'charlie@example.com',
-        currency: 'GBP',
-      },
-      {
-        name: 'Diana',
-        email: 'diana@example.com',
-        currency: 'JPY',
-      },
-      {
-        name: 'Evan',
-        email: 'evan@example.com',
-        currency: 'CNY',
-      },
-    ],
+    data: data
+  })
+  return prisma.user.findMany({
+    orderBy: {
+      id: 'asc'
+    }
+  });
+}
+
+async function createGroups(users) {
+  if (users.length) {
+    for (let i = 0; i < 100; i++) {
+      const s = i * 10
+      const e = ((i + 1)) * 10 - 1
+
+
+      // group of 10
+      const group = await prisma.group.create({
+        data: {
+          name: `Group_10_${i}`,
+          publicId: `Group-10-${i}`,
+          defaultCurrency: 'EUR',
+          userId: users[s].id
+        }
+      });
+
+      await prisma.groupUser.createMany({
+        data: users.slice(s, e).map(u => {
+          return {
+            groupId: group.id,
+            userId: u.id
+          }
+        })
+      });
+
+    }
+  }
+
+  const group = await prisma.group.create({
+    data: {
+      name: `Group_30`,
+      publicId: `Group-30`,
+      defaultCurrency: 'EUR',
+      userId: users[0].id
+    }
   });
 
-  return prisma.user.findMany();
-}
-
-async function createGroups() {
-  // Assuming Alice creates a group and adds Bob and Charlie
-
-  const users = await prisma.user.findMany();
-
-  if (users.length) {
-    const group = await prisma.group.create({
-      data: {
-        name: 'Holiday Trip',
-        publicId: 'holiday-trip-123',
-        defaultCurrency: 'USD',
-        createdBy: { connect: { id: users[0]?.id } },
-      },
-    });
-
-    await prisma.groupUser.createMany({
-      data: users.map((u) => ({ groupId: group.id, userId: u.id })),
-    });
-    console.log('Group created and users added');
-
-    return group;
-  }
-}
-
-async function createExpenses(group, users) {
-  for (let i = 0; i < 100000; i++) {
-    const amount = BigInt(randomInt(1000, 10000));
-    const expense = await prisma.expense.create({
-      data: {
-        name: `Expense ${i}`,
-        paidBy: users[0].id,
-        addedBy: users[0].id,
-        category: 'general',
-        currency: 'USD',
-        amount: amount,
+  await prisma.groupUser.createMany({
+    data: users.slice(0, 29).map(u => {
+      return {
         groupId: group.id,
-        splitType: SplitType.EQUAL,
-      },
-    });
+        userId: u.id
+      }
+    })
+  });
 
-    await prisma.expenseParticipant.createMany({
-      data: [
-        {
-          expenseId: expense.id,
-          userId: users[0].id,
+  return prisma.group.findMany({
+    include: {
+      groupUsers: true
+    }
+  })
+}
+
+async function createExpenses(groups) {
+  const currencies = ['EUR', 'USD']
+  for (const gid in groups) {
+    const group = groups[gid]
+    for (let i = 0; i < 10000; i++) {
+      const c = randomInt(0,2)
+      const amount = BigInt(randomInt(1000, 10000));
+
+      const expense = await prisma.expense.create({
+        data: {
+          name: `Expense Group ${group.id} ${i}`,
+          paidBy: group.groupUsers[0].userId,
+          addedBy: group.groupUsers[0].userId,
+          category: 'general',
+          currency: currencies[c],
           amount: amount,
-        },
-        {
-          expenseId: expense.id,
-          userId: users[1].id,
-          amount: -amount / BigInt(5),
-        },
-        {
-          expenseId: expense.id,
-          userId: users[2].id,
-          amount: -amount / BigInt(5),
-        },
-        {
-          expenseId: expense.id,
-          userId: users[3].id,
-          amount: -amount / BigInt(5),
-        },
-        {
-          expenseId: expense.id,
-          userId: users[4].id,
-          amount: -amount / BigInt(5),
-        },
-      ],
-    });
+          groupId: group.id,
+          splitType: SplitType.EQUAL,
+        }
+      });
+
+      await prisma.expenseParticipant.createMany({
+        data: [
+          { expenseId: expense.id, userId: group.groupUsers[0].userId, amount: amount },
+          ...group.groupUsers.slice(1).map(u => {
+            return {
+              expenseId: expense.id,
+              userId: u.userId,
+              amount: -amount / BigInt(group.groupUsers.length),
+            }
+          })]
+      });
+    }
   }
-  console.log('Expenses added');
 }
 
 async function main() {
   const users = await createUsers();
-  const group = await createGroups();
-  await createExpenses(group, users);
+  const groups = await createGroups(users);
+  await createExpenses(groups);
+  console.log("Seeded db with users, groups and expenses")
 }
 
 main()

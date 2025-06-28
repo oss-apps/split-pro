a662aa44235027525cead85bb135b9abff82d6fb
krokosik <wikkra@gmail.com>
2025-06-28 18:16:50 +0200

    Replace any with unknown

--- src/lib/type.ts
+++ src/lib/type.ts
@@ -1,13 +1,13 @@
 // https://dev.to/lucianbc/union-type-merging-in-typescript-9al
 type CommonKeys<T extends object> = keyof T;
 
-type AllKeys<T> = T extends any ? keyof T : never;
+type AllKeys<T> = T extends unknown ? keyof T : never;
 
 type Subtract<A, C> = A extends C ? never : A;
 
 type NonCommonKeys<T extends object> = Subtract<AllKeys<T>, CommonKeys<T>>;
 
-type PickType<T, K extends AllKeys<T>> = T extends { [k in K]?: any } ? T[K] : undefined;
+type PickType<T, K extends AllKeys<T>> = T extends { [k in K]?: unknown } ? T[K] : undefined;
 
 type PickTypeOf<T, K extends string | number | symbol> =
   K extends AllKeys<T> ? PickType<T, K> : never;

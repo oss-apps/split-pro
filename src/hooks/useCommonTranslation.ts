9ae472a051dc54c51eaba0354ccf39565b1b274a
krokosik <krokosik@pm.me>
2025-08-23 17:12:20 +0200

    Fix typing of useTranslationWithUtils

--- src/hooks/useCommonTranslation.ts
+++ src/hooks/useCommonTranslation.ts
@@ -3,7 +3,9 @@ import { useTranslation } from 'next-i18next';
 import { useCallback } from 'react';
 import { displayName as dn } from '~/utils/strings';
 
-export const useTranslationWithUtils = (namespaces?: string[]) => {
+export const useTranslationWithUtils = (
+  namespaces?: string[],
+): ReturnType<typeof useTranslation> & { displayName: typeof displayName } => {
   if (!namespaces || namespaces.length === 0) {
     namespaces = ['common'];
   } else if (!namespaces.includes('common')) {
@@ -16,6 +18,8 @@ export const useTranslationWithUtils = (namespaces?: string[]) => {
       dn(user, currentUserId, translation.t),
     [translation.t],
   );
+
+  // @ts-ignore
   return {
     ...translation,
     displayName,

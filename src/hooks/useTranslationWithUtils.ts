e19f27cbbd4263901f8aa4a90a0a794d1591e6e8
krokosik <krokosik@pm.me>
2025-08-24 22:10:34 +0200

    Simplify useTranslationWithUtils properly

--- src/hooks/useTranslationWithUtils.ts
+++ src/hooks/useTranslationWithUtils.ts
@@ -1,12 +1,18 @@
 import { useTranslation } from 'next-i18next';
-import { useMemo } from 'react';
-import { displayName, generateSplitDescription } from '~/utils/strings';
+import { useCallback, useMemo } from 'react';
+import {
+  type ParametersExceptTranslation,
+  displayName as dn,
+  generateSplitDescription as gsd,
+  toUIDate as tUD,
+} from '~/utils/strings';
 
 export const useTranslationWithUtils = (
   namespaces?: string[],
 ): ReturnType<typeof useTranslation> & {
   displayName: typeof displayName;
   generateSplitDescription: typeof generateSplitDescription;
+  toUIDate: typeof toUIDate;
 } => {
   if (!namespaces || namespaces.length === 0) {
     namespaces = ['common'];
@@ -15,13 +21,24 @@ export const useTranslationWithUtils = (
   }
   const translation = useTranslation(namespaces);
 
+  const displayName = useCallback(
+    (...args: ParametersExceptTranslation<typeof dn>): string => dn(translation.t, ...args),
+    [translation.t],
+  );
+
+  const generateSplitDescription = useCallback(
+    (...args: ParametersExceptTranslation<typeof gsd>): string => gsd(translation.t, ...args),
+    [translation.t],
+  );
+
+  const toUIDate = useCallback(
+    (...args: ParametersExceptTranslation<typeof tUD>): string => tUD(translation.t, ...args),
+    [translation.t],
+  );
+
   // @ts-ignore
   return useMemo(
-    () => ({
-      ...translation,
-      displayName,
-      generateSplitDescription,
-    }),
-    [translation],
+    () => ({ ...translation, displayName, generateSplitDescription, toUIDate }),
+    [translation, displayName, generateSplitDescription, toUIDate],
   );
 };

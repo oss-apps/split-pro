65d3a489285fc9eeddd44560e59a78f882be5b91
Wiktor Krokosz <38408316+krokosik@users.noreply.github.com>
2025-08-24 22:01:36 +0200

    Simplify useTranslationWithUtils hook using useMemo

--- src/hooks/useTranslationWithUtils.ts
+++ src/hooks/useTranslationWithUtils.ts
@@ -1,12 +1,10 @@
-import { type User } from '@prisma/client';
 import { useTranslation } from 'next-i18next';
-import { useCallback } from 'react';
-import { displayName as dn, generateSplitDescription as gsd } from '~/utils/strings';
-import { type AddExpenseState, type Participant } from '~/store/addStore';
+import { useMemo } from 'react';
+import { displayName, generateSplitDescription } from '~/utils/strings';
 
 export const useTranslationWithUtils = (
   namespaces?: string[],
-): ReturnType<typeof useTranslation> & { 
+): ReturnType<typeof useTranslation> & {
   displayName: typeof displayName;
   generateSplitDescription: typeof generateSplitDescription;
 } => {
@@ -17,28 +15,13 @@ export const useTranslationWithUtils = (
   }
   const translation = useTranslation(namespaces);
 
-  const displayName = useCallback(
-    (user?: Pick<User, 'name' | 'email' | 'id'> | null, currentUserId?: number): string =>
-      dn(user, currentUserId, translation.t),
-    [translation.t],
-  );
-
-  const generateSplitDescription = useCallback(
-    (
-      splitType: Parameters<typeof gsd>[0],
-      participants: Parameters<typeof gsd>[1],
-      splitShares: Parameters<typeof gsd>[2],
-      paidBy: Parameters<typeof gsd>[3],
-      currentUser: Parameters<typeof gsd>[4],
-    ): string =>
-      gsd(splitType, participants, splitShares, paidBy, currentUser, translation.t),
-    [translation.t],
-  );
-
   // @ts-ignore
-  return {
-    ...translation,
-    displayName,
-    generateSplitDescription,
-  };
+  return useMemo(
+    () => ({
+      ...translation,
+      displayName,
+      generateSplitDescription,
+    }),
+    [translation],
+  );
 };

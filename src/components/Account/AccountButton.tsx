d0badc3f5c658dbfd9b37ef576bd96a9cc6377c2
krokosik <krokosik@pm.me>
2025-09-04 23:30:36 +0200

    Properly handle classname in accountbutton to work asChild

--- src/components/Account/AccountButton.tsx
+++ src/components/Account/AccountButton.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { Button, type ButtonProps } from '../ui/button';
 import Link from 'next/link';
 import { ChevronRight } from 'lucide-react';
+import { cn } from '~/lib/utils';
 
 export const AccountButton: React.FC<React.PropsWithChildren<ButtonProps> & { href?: string }> = ({
   children,
   href,
+  className,
   ...buttonProps
 }) => (
   <WithLink href={href}>
     <Button
       variant="ghost"
-      className="text-md hover:text-foreground/80 w-full justify-between px-0"
+      className={cn(className, 'text-md hover:text-foreground/80 w-full justify-between px-0')}
       {...buttonProps}
     >
       <div className="flex items-center gap-4">{children}</div>

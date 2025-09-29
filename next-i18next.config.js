e909b000f8b1547de0392620f3f67a8cf3700a90
FelixDz <FelixDz@users.noreply.github.com>
2025-09-29 13:50:48 +0200

    Fixing locales ordering
    Fixing locales ordering : 'en' coming first

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@
 const config = {
   i18n: {
     defaultLocale: 'default',
-    locales: ['default', 'de', 'en', 'fr', 'it', 'pl', 'pt', 'sv', 'es-MX', 'es-AR'],
+    locales: ['default', 'en', 'de', 'fr', 'it', 'pl', 'pt', 'sv', 'es-MX', 'es-AR'],
     localeDetection: false,
   },
   localePath: './public/locales',

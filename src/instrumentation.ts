6c0a22605edddbd8759906fa59a92a36ca4d4946
krokosik <krokosik@pm.me>
2025-10-04 18:15:08 +0200

    Fix peer dep issues and next runtime check

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -4,7 +4,7 @@
  * more details here: https://nextjs.org/docs/app/building-your-application/optimizing/instrumentation
  */
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'edge') {
+  if (process.env.NEXT_RUNTIME !== 'nodejs') {
     console.log('Skipping instrumentation on edge runtime');
     return;
   }

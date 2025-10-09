e3afbd3f5bc775129b85187328a375259ecc8325
krokosik <krokosik@pm.me>
2025-10-09 23:24:02 +0200

    Add BA graph generator to friends relations

--- src/utils/dummies.ts
+++ src/utils/dummies.ts
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker';
-import { SplitType, type User } from '@prisma/client';
+import { type User } from '@prisma/client';
 
 const SEED = 2137;
 
@@ -19,10 +19,20 @@ const weightedCurrencies = [
   ...oddCurrencies.map((value) => ({ weight: 0.05, value })),
 ];
 
+const gaussianRandom = (mean = 0, stdev = 1) => {
+  const u = 1 - faker.number.float({ min: 0, max: 1 }); // Converting [0,1) to (0,1)
+  const v = faker.number.float({ min: 0, max: 1 });
+  const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+  // Transform to the desired mean and standard deviation:
+  return z * stdev + mean;
+};
+
 export const dummyUsers = (() => {
   const users: User[] = [];
+  const adjList: number[][] = [];
+  const m = 2;
 
-  while (users.length < 10_000) {
+  while (users.length < 1_000) {
     const firstName = faker.person.firstName();
     const lastName = faker.person.lastName();
     const name = `${firstName} ${lastName}`;
@@ -40,18 +50,33 @@ export const dummyUsers = (() => {
       obapiProviderId: null,
       preferredLanguage: 'en',
     });
+
+    // Construct Barabasi-Albert graph to simulate social connections
+    // Each new node connects to m existing nodes with a probability proportional to their degree
+    // This creates a scale-free network with some highly connected hubs
+    // Reference: https://en.wikipedia.org/wiki/Barab%C3%A1si%E2%80%93Albert_model
+    if (users.length > m) {
+      const targets = new Set<number>();
+      const degreeSum = adjList.reduce((sum, adj) => sum + adj.length, 0);
+      while (targets.size < m) {
+        for (let i = 0; i < adjList.length; i++) {
+          const prob = adjList[i]!.length / degreeSum;
+          if (faker.number.float({ min: 0, max: 1 }) < prob) {
+            targets.add(i);
+            if (targets.size >= m) {
+              break;
+            }
+          }
+        }
+      }
+      const newNodeIndex = adjList.length;
+      adjList.push(Array.from(targets));
+      targets.forEach((t) => adjList[t]!.push(newNodeIndex));
+    }
   }
 
   return users;
 })();
-
-const gaussianRandom = (mean = 0, stdev = 1) => {
-  const u = 1 - faker.number.float({ min: 0, max: 1 }); // Converting [0,1) to (0,1)
-  const v = faker.number.float({ min: 0, max: 1 });
-  const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
-  // Transform to the desired mean and standard deviation:
-  return z * stdev + mean;
-};
 
 export const groups = (() => {
   const getGroupMemberCount = () => {
@@ -145,3 +170,17 @@ export const groups = (() => {
     }
   }
 })();
+
+// {
+//     name: 'Dinner at local ramen shop',
+//     category: 'diningOut',
+//     amount: 6800,
+//     currency: 'JPY',
+//     splitType: SplitType.EQUAL,
+//     paidByIndex: 0,
+//     participantIndices: [0, 3, 4],
+//   },
+
+const expenseGenerator = () => {
+  const expenses = [];
+};

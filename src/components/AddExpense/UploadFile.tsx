4ab4b3311695f70e48a66a499ddb3647ebbe7a22
Wiktor Krokosz <38408316+krokosik@users.noreply.github.com>
2025-10-16 20:25:39 +0200

    Fix receipt missing on edit (#431)
    Apply fixes from PR #417

--- src/components/AddExpense/UploadFile.tsx
+++ src/components/AddExpense/UploadFile.tsx
@@ -26,62 +26,66 @@ const getImgHeightAndWidth = (file: File) =>
 export const UploadFile: React.FC = () => {
   const { t } = useTranslation();
   const [file, setFile] = useState<File | null>(null);
+  const fileKey = useAddExpenseStore((s) => s.fileKey);
   const { setFileUploading, setFileKey } = useAddExpenseStore((s) => s.actions);
 
   const getUploadUrl = api.expense.getUploadUrl.useMutation();
 
-  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { files } = event.target;
+  const handleFileChange = React.useCallback(
+    async (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { files } = event.target;
 
-    const file = files?.[0];
+      const file = files?.[0];
 
-    if (!file) {
-      return;
-    }
-
-    if (file.size > FILE_SIZE_LIMIT) {
-      toast.error(`${t('errors.less_than')} ${FILE_SIZE_LIMIT / 1024 / 1024}MB`);
-      return;
-    }
-
-    setFile(file);
-
-    await getImgHeightAndWidth(file);
-
-    setFileUploading(true);
-    try {
-      const { fileUrl, key } = await getUploadUrl.mutateAsync({
-        fileName: file.name,
-        fileType: file.type,
-        fileSize: file.size,
-      });
-
-      const response = await fetch(fileUrl, {
-        method: 'PUT',
-        body: file,
-      });
-
-      if (!response.ok) {
-        toast.error(t('errors.upload_failed'));
-        console.error('Failed to upload file:', response.statusText);
-        setFile(null);
+      if (!file) {
         return;
       }
 
-      toast.success(t('expense_details.add_expense_details.upload_file.messages.upload_success'));
+      if (file.size > FILE_SIZE_LIMIT) {
+        toast.error(`${t('errors.less_than')} ${FILE_SIZE_LIMIT / 1024 / 1024}MB`);
+        return;
+      }
 
-      setFileKey(key);
-    } catch (error) {
-      console.error('Error getting upload url:', error);
-      toast.error(t('errors.uploading_error'));
-    } finally {
-      setFileUploading(false);
-    }
-  };
+      setFile(file);
+
+      await getImgHeightAndWidth(file);
+
+      setFileUploading(true);
+      try {
+        const { fileUrl, key } = await getUploadUrl.mutateAsync({
+          fileName: file.name,
+          fileType: file.type,
+          fileSize: file.size,
+        });
+
+        const response = await fetch(fileUrl, {
+          method: 'PUT',
+          body: file,
+        });
+
+        if (!response.ok) {
+          toast.error(t('errors.upload_failed'));
+          console.error('Failed to upload file:', response.statusText);
+          setFile(null);
+          return;
+        }
+
+        toast.success(t('expense_details.add_expense_details.upload_file.messages.upload_success'));
+
+        setFileKey(key);
+      } catch (error) {
+        console.error('Error getting upload url:', error);
+        toast.error(t('errors.uploading_error'));
+      } finally {
+        setFileUploading(false);
+      }
+    },
+    [getUploadUrl, setFileUploading, setFileKey, t],
+  );
 
   return (
     <Label htmlFor="picture" className="cursor-pointer">
-      {file ? (
+      {file || fileKey ? (
         <ImageUploaded className="text-primary h-6 w-6" />
       ) : (
         <ImagePlus className="h-6 w-6 text-gray-300" />

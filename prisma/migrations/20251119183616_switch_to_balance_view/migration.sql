2878e016c3f49f3d4522139c89dc7c7c69b12ac4
krokosik <krokosik@pm.me>
2025-11-19 19:53:23 +0100

    Update migration to preserve balance data

--- prisma/migrations/20251119183616_switch_to_balance_view/migration.sql
+++ prisma/migrations/20251119183616_switch_to_balance_view/migration.sql
@@ -1,10 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Balance` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `GroupBalance` table. If the table is not empty, all the data it contains will be lost.
-
-*/
 -- DropForeignKey
 ALTER TABLE "public"."Balance" DROP CONSTRAINT "Balance_friendId_fkey";
 
@@ -19,9 +12,3 @@ ALTER TABLE "public"."GroupBalance" DROP CONSTRAINT "GroupBalance_groupId_fkey";
 
 -- DropForeignKey
 ALTER TABLE "public"."GroupBalance" DROP CONSTRAINT "GroupBalance_userId_fkey";
-
--- DropTable
-DROP TABLE "public"."Balance";
-
--- DropTable
-DROP TABLE "public"."GroupBalance";

4d1495b8c6ee8cee04fd5e15bf7f8b6c38d9360b
krokosik <krokosik@pm.me>
2025-11-30 19:58:32 +0100

    Fix cut command dialog

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -57,7 +57,7 @@ const CommandList = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <CommandPrimitive.List
     ref={ref}
-    className={cn('max-h-[300px] overflow-x-hidden overflow-y-auto', className)}
+    className={cn('overflow-x-hidden overflow-y-auto', className)}
     {...props}
   />
 ));

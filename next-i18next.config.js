24d8a5d66c96e57f0020de0a9ea7bf2b95eb2d36
bekdan1988 <151082017+bekdan1988@users.noreply.github.com>
2026-01-19 13:29:05 +0100

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,11 +1,23 @@
 /** @type {import('next-i18next').UserConfig} */
 const config = {
   i18n: {
-    defaultLocale: 'default',
-    locales: ['default', 'en', 'de', 'fr', 'it', 'pl', 'pt-PT', 'pt-BR', 'sv', 'es-MX', 'es-AR'],
+    defaultLocale: "default",
+    locales: [
+      "default",
+      "en",
+      "de",
+      "fr",
+      "it",
+      "pl",
+      "pt-PT",
+      "pt-BR",
+      "sv",
+      "es-MX",
+      "es-AR",
+    ],
     localeDetection: false,
   },
-  localePath: './public/locales',
+  localePath: "./public/locales",
 };
 
 export default config;

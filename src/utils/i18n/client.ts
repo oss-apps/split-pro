dd0962a0e48512169ac2bb5b1edb8f8e7d88c466
bekdan1988 <151082017+bekdan1988@users.noreply.github.com>
2026-01-19 12:57:58 +0100

    Add Hungarian language support

--- src/utils/i18n/client.ts
+++ src/utils/i18n/client.ts
@@ -7,6 +7,7 @@ export const getSupportedLanguages = (): SupportedLanguage[] => [
   { code: 'en', name: 'English' },
   { code: 'fr', name: 'Français' },
   { code: 'de', name: 'Deutsch' },
+  { code: 'hu', name: 'Magyar' },
   { code: 'it', name: 'Italiano' },
   { code: 'pl', name: 'Polski' },
   { code: 'pt-BR', name: 'Português (Brasil)' },

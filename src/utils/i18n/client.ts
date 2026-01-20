80994c797fc293cf0804d85733b6c2be2a1b4494
krokosik <krokosik@pm.me>
2026-01-20 14:53:32 +0100

    Add hungarian and sort locales

--- src/utils/i18n/client.ts
+++ src/utils/i18n/client.ts
@@ -3,16 +3,18 @@ export interface SupportedLanguage {
   name: string;
 }
 
-export const getSupportedLanguages = (): SupportedLanguage[] => [
-  { code: 'en', name: 'English' },
-  { code: 'fr', name: 'Français' },
-  { code: 'de', name: 'Deutsch' },
-  { code: 'it', name: 'Italiano' },
-  { code: 'pl', name: 'Polski' },
-  { code: 'pt-BR', name: 'Português (Brasil)' },
-  { code: 'pt-PT', name: 'Português (Portugal)' },
-  { code: 'es', name: 'Español' },
-  { code: 'es-MX', name: 'Español (Mexico)' },
-  { code: 'es-AR', name: 'Español (Argentina)' },
-  { code: 'sv', name: 'Svenska' },
-];
+export const getSupportedLanguages = (): SupportedLanguage[] =>
+  [
+    { code: 'en', name: 'English' },
+    { code: 'fr', name: 'Français' },
+    { code: 'de', name: 'Deutsch' },
+    { code: 'it', name: 'Italiano' },
+    { code: 'pl', name: 'Polski' },
+    { code: 'pt-BR', name: 'Português (Brasil)' },
+    { code: 'pt-PT', name: 'Português (Portugal)' },
+    { code: 'es', name: 'Español' },
+    { code: 'es-MX', name: 'Español (Mexico)' },
+    { code: 'es-AR', name: 'Español (Argentina)' },
+    { code: 'sv', name: 'Svenska' },
+    { code: 'hu', name: 'Magyar' },
+  ].sort((a, b) => a.name.localeCompare(b.name));

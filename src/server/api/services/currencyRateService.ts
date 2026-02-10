7be8f60a6bfe52f8ae9389bdb67e19bebf6c36f9
krokosik <krokosik@pm.me>
2026-02-10 22:57:19 +0100

    Remove stray console log

--- src/server/api/services/currencyRateService.ts
+++ src/server/api/services/currencyRateService.ts
@@ -27,7 +27,6 @@ abstract class CurrencyRateProvider {
 
     const cachedRate = await this.checkCache(from, to, date);
     if (cachedRate) {
-      console.log(cachedRate);
       return cachedRate;
     }
 
